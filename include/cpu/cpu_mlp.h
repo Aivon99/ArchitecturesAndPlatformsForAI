f04d03ff5ae9989f0ec6c6e19b7233d39c0351ca
Aivon99 <109174292+Aivon99@users.noreply.github.com>
2026-05-20 17:53:33 +0800

    base .h of cpu mlp

---
+++ include/cpu/cpu_mlp.h
@@ -0,0 +1,20 @@
+
+
+#ifndef CPU_MLP_H
+#define CPU_MLP_H
+
+#include <vector>
+
+class CPUMlp {
+private:
+    int input_size;
+    int output_size;
+    std::vector<float> weights;
+    std::vector<float> biases;
+public:
+    CPUMlp(int in_size, int out_size);
+
+    std::vector<float> forward(const std::vector<float>& input);
+};
+
+#endif 
